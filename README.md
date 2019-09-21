c43ba0b637e7e8b76ebf29483cfde065c304b179
johanse7 <johanrodriguez_14@hotmail.com>
2019-09-20 22:40:59 -0500

    platzi video
    primer commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# PlatziVideo
-Platzi video studio
+# escuela-js
---
+++ src/hooks/useTvShowsApi.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react';
+
+const useTvShowsApi = url => {
+  const [tvShows, setTvShows] = useState([]);
+  useEffect(() => {
+      fetch(url)
+      .then(response => response.json())
+      .then(data => setTvShows(data));
+  }, []);
+  return tvShows;
+};
+
+export default useTvShowsApi;
